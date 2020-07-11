ecde851905b35c0198dc1531a0b75eeba290ac3d
Nishant garg <iamnishantgarg@gmail.com>
2020-07-11 19:37:10 +0530

    comments add delete one

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -76,7 +76,6 @@ router.get("/:post_id", auth, async (req, res) => {
 //@access   Private
 router.delete("/:post_id", auth, async (req, res) => {
   try {
-    // await Post.findByIdAndRemove(req.params.post_id);
     const post = await Post.findById(req.params.post_id);
     if (!post) {
       return res.status(404).json({ msg: "No post found " });
@@ -92,7 +91,7 @@ router.delete("/:post_id", auth, async (req, res) => {
       return res.status("404").json({ msg: "No post found" });
     return res.status(500).send("server error");
   }
-});
+}); // await Post.findByIdAndRemove(req.params.post_id);
 
 //@route    PUT /api/posts/like/:id
 //@desc     Like a post
@@ -156,6 +155,77 @@ router.put("/unlike/:id", auth, async (req, res) => {
       return res.status(404).json({ msg: "No post found " });
     }
     return res.status(500).send("Server Error");
+  }
+});
+
+//@route    POST /api/posts/comment:id
+//@desc     post a comment
+//@access   Private
+router.post(
+  "/comment/:id",
+  auth,
+  [check("text", "Text is required").not().isEmpty()],
+  async (req, res) => {
+    // console.log("heekfweeio");
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findOne({ _id: req.user.id }).select("-password");
+      const post = await Post.findById(req.params.id);
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      post.comments.unshift(newComment);
+      // const post = new Post(newPost);
+      await post.save();
+      return res.json(post.comments);
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).send("Server Error");
+    }
+  }
+);
+
+//@route    DELETE /api/posts/comment/:id/:comment_id
+//@desc     Delete comment by id
+//@access   Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "No post found " });
+    }
+    //Pull out a comment
+    const comment = post.comments.find(
+      (comment) => comment._id.toString() === req.params.comment_id
+    );
+    //If comment exists;
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exist" });
+    }
+    //check if comment belong to user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+    //remove
+    const removeIndex = post.comments
+      .map((comment) => comment.user._id.toString())
+      .indexOf(req.user.comment_id);
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+    return res.json(post.comments);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === "ObjectId")
+      return res.status("404").json({ msg: "No post found" });
+    return res.status(500).send("server error");
   }
 });
 
