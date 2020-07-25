97e415dd790ddecce1926899c46bb5597ab63c3e
Nishant garg <iamnishantgarg@gmail.com>
2020-07-25 12:48:17 +0530

    client landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
+  );
 }
 
 export default App;
