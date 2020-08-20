ae458fdbc8d24a4e1a6b8b659328e0ce07bee8a0
Nishant garg <iamnishantgarg@gmail.com>
2020-08-20 16:17:08 +0530

    education and experience added

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,8 @@ import Spinner from "../layout/Spinner";
 import { Link } from "react-router-dom";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
+import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
 const Profile = ({
   profile: { profile, loading },
   getProfileById,
@@ -34,6 +36,30 @@ const Profile = ({
           <div class="profile-grid my-1">
             <ProfileTop profile={!loading && profile} />
             <ProfileAbout profile={!loading && profile} />
+            <div className="profile-exp bg-white p-2">
+              <h2 class="text-primary">Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map((exp) => (
+                    <ProfileExperience key={exp._id} experience={exp} />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credetials</h4>
+              )}
+            </div>
+            <div className="profile-edu bg-white p-2">
+              <h2 class="text-primary">Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map((edu) => (
+                    <ProfileEducation key={edu._id} education={edu} />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credetials</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
---
+++ client/src/components/profile/ProfileExperience.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Moment from "react-moment";
+
+const ProfileExperience = ({
+  experience: { company, from, to, current, description, title },
+}) => {
+  return (
+    <div>
+      <h3 class="text-dark">{company}</h3>
+      <p>
+        {from && <Moment format="YYYY/MM/DD">{from}</Moment>} -{" "}
+        {current ? <span>Now</span> : <Moment format="YYYY/MM/DD">{to}</Moment>}
+      </p>
+      <p>
+        <strong>Position: </strong>
+        {title}
+      </p>
+      <p>
+        <strong>Description: </strong>
+        {description && <span>{description}</span>}
+      </p>
+    </div>
+  );
+};
+
+ProfileExperience.propTypes = {
+  experience: PropTypes.object.isRequired,
+};
+
+export default ProfileExperience;
